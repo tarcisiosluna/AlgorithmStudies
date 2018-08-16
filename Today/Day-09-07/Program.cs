84d9591693c684dc4e2f8d12c7401a70d617f610
Tarcisio Luna <tarcisiosluna@gmail.com>
2018-08-15 22:38:38 -0300

    Add problem description

--- Today/Day-09-07/Program.cs
+++ Today/Day-09-07/Program.cs
@@ -4,6 +4,11 @@ namespace Day_09_07
 {
     class Program
     {
+        /*
+            Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+            or example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+            Follow-up: what if you can't use division?
+        */
         static void Main(string[] args)
         {
             int[] input = { 3,2,1 };
