a1f63d151a768df4a2975fcc30929f1718e0ae11
Tarcisio Luna <tarcisiosluna@gmail.com>
2018-08-19 16:10:26 -0300

    SF

--- Today/Day-12-08/Program.cs
+++ Today/Day-12-08/Program.cs
@@ -5,11 +5,9 @@ namespace Day_12_08
     class Program
     {
         /*
-Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
-
-Do this in linear time and in-place.
-
-For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+        Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. 
+        You can only swap elements of the array. Do this in linear time and in-place.
+        For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
         */
 
         static char[] input = {'G', 'B', 'R', 'R', 'B', 'R', 'R', 'G'};
---
+++ Today/Day-18-08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day_18_08
+{
+    class Program
+    {
+        /*
+            Given an array of integers where every integer occurs three times except for one integer, which only occurs once, find and return the non-duplicated integer.
+        */
+        static void Main(string[] args)
+        {
+            int[] array = {6, 1, 3, 3, 3, 6, 6};
+            FindNonDuplicated(array);
+        }
+
+        private static int FindNonDuplicated(int[] array)
+        {
+            int result = 0;
+            int[] counterArray = new int[short.MaxValue];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                counterArray[array[i]] += 1;
+            }
+
+            for (int i = 0; i < counterArray.Length; i++)
+            {
+                if (counterArray[i] != 0 && counterArray[i] < 3)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
